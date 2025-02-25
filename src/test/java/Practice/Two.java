51a3968d68b1509ae3d8ab7baee2504a2d94180f
Kanimozhi <107602855+KanimozhiJagannathan22@users.noreply.github.com>
2025-02-25 15:25:05 +0530

    Update Two.java

--- src/test/java/Practice/Two.java
+++ src/test/java/Practice/Two.java
@@ -3,6 +3,9 @@ package Practice;
 public class Two {
 	public static void main(String[] args) {
 		System.out.println("Hi");
+		System.out.println("Hello");
+		System.out.println("Hell");
+		
 	}
 
 }
